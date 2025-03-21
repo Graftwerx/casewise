357e768ef645ae30fd69357b75bde9a0a5ada905
Julian Odilibe <graftwerx@gmail.com>
2025-03-21 14:03:24 +0000

    rnd

---
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -0,0 +1,86 @@
+"use client";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import HandleComponent from "@/components/ui/HandleComponent";
+import { cn } from "@/lib/utils";
+import NextImage from "next/image";
+import React from "react";
+import { Rnd } from "react-rnd";
+
+interface DesignConfiguratorProps {
+  configId: string;
+  imageUrl: string;
+  imageDimensions: { width: number; height: number };
+}
+
+const DesignConfigurator = ({
+  configId,
+  imageUrl,
+  imageDimensions,
+}: DesignConfiguratorProps) => {
+  return (
+    <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
+      <div
+        className="relative h-[37.5rem] overflow-hidden col-span-2 w-full 
+                        max-w-4xl flex items-center justify-center rounded-lg border-2
+                        border-dashed border-gray-300 p-12 text-center focus:outline-none
+                        focus:ring-primary focus:ring-offset-2"
+      >
+        <div
+          className="relative w-60 bg-opacity-50 pointer-events-none
+                            aspect-[896/1831]"
+        >
+          <AspectRatio
+            ratio={896 / 1831}
+            className="relative pointer-events-none
+                                z-50 aspect-[896/1831] w-full"
+          >
+            <NextImage
+              fill
+              alt="phone"
+              src="/phone-template.png"
+              className="z-50 pointer-events-none
+                                    select-none"
+            />
+          </AspectRatio>
+          <div
+            className="absolute z-40 inset-0 left-[3px] top-px right-[3px]
+                          bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]"
+          />
+          <div
+            className={cn(
+              "absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]",
+              `bg-blue-950`
+            )}
+          />
+        </div>
+        <Rnd
+          default={{
+            x: 150,
+            y: 205,
+            height: imageDimensions.height / 4,
+            width: imageDimensions.width / 4,
+          }}
+          className="absolute z-20 border-[3px] border-primary"
+          lockAspectRatio
+          resizeHandleComponent={{
+            bottomRight: <HandleComponent />,
+            bottomLeft: <HandleComponent />,
+            topRight: <HandleComponent />,
+            topLeft: <HandleComponent />,
+          }}
+        >
+          <div className="relative w-full h-full">
+            <NextImage
+              fill
+              src={imageUrl}
+              alt="uploadedImage"
+              className="pointer-events-none"
+            />
+          </div>
+        </Rnd>
+      </div>
+    </div>
+  );
+};
+
+export default DesignConfigurator;
