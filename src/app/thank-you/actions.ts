6af87f1cf85c1b5ca4ef7c7439e886a22eb3ca75
Julian Odilibe <graftwerx@gmail.com>
2025-04-02 16:34:38 +0100

    revert changes redirect

--- src/app/thank-you/actions.ts
+++ src/app/thank-you/actions.ts
@@ -1,39 +1,33 @@
-import { db } from "@/db";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+'use server'
+
+import { db } from '@/db'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 export const getPaymentStatus = async ({ orderId }: { orderId: string }) => {
-    console.log("🔍 Checking payment status for order:", orderId);
-  
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
-  
-    if (!user?.id || !user.email) {
-      console.error("🚫 Not logged in");
-      throw new Error("You need to be logged in to view this page.");
-    }
-  
-    const order = await db.order.findFirst({
-      where: { id: orderId, userId: user.id },
-      include: {
-        billingAddress: true,
-        configuration: true,
-        shippingAddress: true,
-        user: true,
-      },
-    });
-  
-    if (!order) {
-      console.error("🚫 No order found for user:", user.id);
-      throw new Error("This order does not exist.");
-    }
-  
-    if (order.isPaid) {
-      console.log("✅ Order is paid:", order.id);
-      return order;
-    } else {
-      console.log("⏳ Order not paid yet:", order.id);
-      return false;
-    }
-  };
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
+  if (!user?.id || !user.email) {
+    throw new Error('You need to be logged in to view this page.')
+  }
+
+  const order = await db.order.findFirst({
+    where: { id: orderId, userId: user.id },
+    include: {
+      billingAddress: true,
+      configuration: true,
+      shippingAddress: true,
+      user: true,
+    },
+  })
+
+  if (!order) throw new Error('This order does not exist.')
+
+  if (order.isPaid) {
+    return order
+  } else {
+    return false
+  }
+}
   
 
