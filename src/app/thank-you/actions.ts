da472b0c822db4d62b583427f7c1dcebd84fa863
Julian Odilibe <graftwerx@gmail.com>
2025-04-03 09:17:12 +0100

    c2d

--- src/app/thank-you/actions.ts
+++ src/app/thank-you/actions.ts
@@ -3,6 +3,10 @@
 import { db } from '@/db'
 // import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
+
+
+
+
 export const getPaymentStatus = async ({ orderId }: { orderId: string }) => {
   const order = await db.order.findUnique({
     where: { id: orderId },
@@ -13,9 +17,21 @@ export const getPaymentStatus = async ({ orderId }: { orderId: string }) => {
     },
   })
 
-  if (!order) throw new Error("Order not found")
+  if (!order) {
+    throw new Error('This order does not exist.')
+  }
 
-  return order.isPaid ? order : false
+  // You can also limit what gets returned here for security
+  if (order.isPaid) {
+    return {
+      configuration: order.configuration,
+      billingAddress: order.billingAddress,
+      shippingAddress: order.shippingAddress,
+      amount: order.amount,
+    }
+  } else {
+    return false
+  }
 }
 
 
