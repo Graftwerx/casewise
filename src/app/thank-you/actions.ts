6ab4d932aad28743953883f0cf901a53202cbe1f
Julian Odilibe <graftwerx@gmail.com>
2025-04-03 09:06:46 +0100

    commit 2 deploy

--- src/app/thank-you/actions.ts
+++ src/app/thank-you/actions.ts
@@ -1,33 +1,21 @@
 'use server'
 
 import { db } from '@/db'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+// import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 export const getPaymentStatus = async ({ orderId }: { orderId: string }) => {
-  const { getUser } = getKindeServerSession()
-  const user = await getUser()
-
-  if (!user?.id || !user.email) {
-    throw new Error('You need to be logged in to view this page.')
-  }
-
-  const order = await db.order.findFirst({
-    where: { id: orderId, userId: user.id },
+  const order = await db.order.findUnique({
+    where: { id: orderId },
     include: {
       billingAddress: true,
-      configuration: true,
       shippingAddress: true,
-      user: true,
+      configuration: true,
     },
   })
 
-  if (!order) throw new Error('This order does not exist.')
+  if (!order) throw new Error("Order not found")
 
-  if (order.isPaid) {
-    return order
-  } else {
-    return false
-  }
+  return order.isPaid ? order : false
 }
-  
+
 
