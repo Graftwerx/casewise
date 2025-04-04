59d4f5468d55dbcb19aa62a52250711883c93aad
Julian Odilibe <graftwerx@gmail.com>
2025-04-04 09:38:02 +0100

    shadcn tables

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,6 +7,14 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { db } from "@/db";
 import { formatPrice } from "@/lib/utils";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
@@ -50,8 +58,20 @@ const Page = async () => {
       amount: true,
     },
   });
+  const lastMonthSum = await db.order.aggregate({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 30)),
+      },
+    },
+    _sum: {
+      amount: true,
+    },
+  });
 
   const WEEKLY_GOAL = 500;
+  const MONTHLY_GOAL = 2200;
 
   return (
     <div className="flex min-h-screen w-full bg-muted/40">
@@ -76,7 +96,77 @@ const Page = async () => {
                 />
               </CardFooter>
             </Card>
+            <Card>
+              <CardHeader className="pb-2">
+                <CardDescription>Last Month</CardDescription>
+                <CardTitle className="text-4xl">
+                  {formatPrice(lastMonthSum._sum.amount ?? 0)}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-sm text-muted-foreground">
+                  of {formatPrice(MONTHLY_GOAL)} goal
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Progress
+                  value={((lastMonthSum._sum.amount ?? 0) * 100) / MONTHLY_GOAL}
+                />
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardHeader className="pb-2">
+                <CardDescription>Last Week</CardDescription>
+                <CardTitle className="text-4xl">
+                  {formatPrice(lastWeekSum._sum.amount ?? 0)}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-sm text-muted-foreground">
+                  of {formatPrice(WEEKLY_GOAL)} goal
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Progress
+                  value={((lastWeekSum._sum.amount ?? 0) * 100) / WEEKLY_GOAL}
+                />
+              </CardFooter>
+            </Card>
           </div>
+          <h1 className="text-4xl font-bold tracking-tight">Incoming orders</h1>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Customer</TableHead>
+                <TableHead className="hidden sm:table-cell">Status</TableHead>
+                <TableHead className="hidden sm:table-cell">
+                  Purchase date
+                </TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {orders.map((order) => (
+                <TableRow key={order.id} className="bg-accent">
+                  <TableCell>
+                    <div className="font-medium">
+                      {order.shippingAddress?.name}
+                    </div>
+                    <div className="hidden text-sm text-muted-foreground md:inline">
+                      {order.user.email}
+                    </div>
+                  </TableCell>
+                  <TableCell>status</TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    {order.createdAt.toLocaleDateString()}
+                  </TableCell>
+                  <TableCell className="text-right">
+                    {formatPrice(order.amount)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </div>
       </div>
     </div>
