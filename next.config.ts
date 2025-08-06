8a9c0444716b9ba18860d636427637e8741aa345
Julian Odilibe <graftwerx@gmail.com>
2025-08-06 06:51:10 +0100

    remote patterns next.config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ['utfs.io', 'vs4yihw6cl.ufs.sh', "llz1c88so4.ufs.sh"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'llz1c88so4.ufs.sh',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
 
